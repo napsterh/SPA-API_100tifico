215ab9ef1dc94b081f4823e4ddc3cb11912da8ff
Napster <rome.1205h@gmail.com>
2020-10-31 12:18:26 -0500

    mconsumiendo el API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+    const characters =  await getData();
     const view = `
         <div class="Characters">
-            <article class="Character-item">
-                <a href="#/1/">
-                    <img src="image" alt="name">
-                    <H2>Nombre</H2>
-                </a>
-            </article>
+            ${characters.results.map(character => `
+                <article class="Character-item">
+                    <a href="#/${character.id}/">
+                        <img src=${character.image} alt=${character.name}>
+                        <H2>${character.name}</H2>
+                    </a>
+                </article>
+            `).join('')}
         </div>
     `;
     return view;
